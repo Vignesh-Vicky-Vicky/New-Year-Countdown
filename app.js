dd8913dff930b363ce73842b622ff6bb26504755
Vignesh-Vicky-Vicky <85555447+Vignesh-Vicky-Vicky@users.noreply.github.com>
2021-06-11 14:21:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+
+let daysEl = document.getElementById("daysinput");
+let hoursEl = document.getElementById("hoursinput");
+let minutesEl = document.getElementById("minutesinput");
+let secondsEl = document.getElementById("secondsinput");
+
+function countdown() {
+    const newyear = new Date('1 Jan 2022');
+    const currenttime = new Date();
+
+    const totalseconds = (newyear - currenttime) / 1000;
+
+    const days = Math.floor(totalseconds / 3600 / 24);
+    const hours = Math.floor(totalseconds / 3600) % 24;
+    const minutes = Math.floor(totalseconds / 60) % 60;
+    const seconds = Math.floor(totalseconds) % 60;
+
+    daysEl.innerHTML = timeformat(days);
+    hoursEl.innerHTML = timeformat(hours);
+    minutesEl.innerHTML = timeformat(minutes);
+    secondsEl.innerHTML = timeformat(seconds);
+
+}
+
+function timeformat(time){
+    return time < 10 ? (`0${time}`) : time;
+}
+
+countdown()
+
+setInterval(countdown, 1000);
